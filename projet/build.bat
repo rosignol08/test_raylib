4d5fc0397116ed6e40a6a90dff1e3537970bef0a
Aleyks <60187415+Aleyks@users.noreply.github.com>
2025-05-31 22:33:21 +0200

    dossier projet supprimé

--- projet/build.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-if not exist build mkdir build
-cd build
-cmake .. -G "MinGW Makefiles"
-mingw32-make
-echo Build termine: build\main.exe
--- projet/src/ecosystem/gridCell.cpp
+++
@@ -1,9 +0,0 @@
-// src/ecosystem/GridCell.cpp
-#include "gridCell.h"
-
-// Constructeur de GridCell
-GridCell::GridCell(int id, Vector3 pos, Model mod, bool act, bool occ, 
-                   int temp, int hum, int plu, float pen, Plante plante)
-    : identifiant(id), position(pos), model(mod), active(act), occupee(occ), 
-      temperature(temp), humidite(hum), pluviometrie(plu), pente(pen), plante(plante) {
-}
